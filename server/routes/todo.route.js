a6d6ce268c6454cf257515d5c13f56fae61ab0c7
Vivek <vivekparde2411@gmail.com>
2023-08-14 12:33:43 +0530

    added todos routes

--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {jwtAuth} = require('../middleware/user.middleware');
-const { createTask } = require('../controllers/todo.controller');
+const { createTask, getAllTodos, deleteTask, updateTask } = require('../controllers/todo.controller');
 const todoRouter = express.Router();
 
 todoRouter.post('/create', jwtAuth, createTask);
+todoRouter.post('/allTodos', jwtAuth, getAllTodos);
+todoRouter.post('/delete', jwtAuth, deleteTask);
+todoRouter.post('/update', jwtAuth, updateTask);
 
 
 module.exports = todoRouter
