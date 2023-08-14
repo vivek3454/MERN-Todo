64cc4a3344abb8aa89ed96e48e11d97ce6ff6864
Vivek <vivekparde2411@gmail.com>
2023-08-14 12:33:02 +0530

    created getAllTasks , updateTask and deleteTask

--- server/controllers/todo.controller.js
+++ server/controllers/todo.controller.js
@@ -17,4 +17,37 @@ const createTask = async (req,res,next)=>{
     })
 }
 
-module.exports = {createTask}
+const getAllTodos = async (req,res,next)=>{
+    const {userId} = req.body;
+    const todos = await todoModel.find({userId});
+    // todos.save();
+
+    return res.status(200).json({
+        success: true,
+        message:"All todos",
+        todos
+    })
+}
+
+const deleteTask = async (req,res,next)=>{
+    const {_id} = req.body;
+    const todos = await todoModel.findByIdAndDelete({_id});
+    // todos.save();
+
+    return res.status(200).json({
+        success: true,
+        message:"Task deleted successfully",
+    })
+}
+const updateTask = async (req,res,next)=>{
+    const {todo, _id} = req.body;
+    const todos = await todoModel.findByIdAndUpdate({_id},{todo});
+    // todos.save();
+
+    return res.status(200).json({
+        success: true,
+        message:"Task updated successfully"
+    })
+}
+
+module.exports = {createTask, getAllTodos, deleteTask, updateTask}
