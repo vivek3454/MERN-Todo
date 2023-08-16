c2888810cc718fab37289c9226d5ee947526a296
Vivek <vivekparde2411@gmail.com>
2023-08-16 18:12:17 +0530

    fixed ui bug

--- client/src/pages/todos/AllTodos.jsx
+++ client/src/pages/todos/AllTodos.jsx
@@ -5,15 +5,13 @@ import axios from 'axios';
 const Todo = () => {
   const [task, setTask] = useState('');
   const [allTodos, setAllTodos] = useState([]);
-  let id;
 
   const fetchAllTask = async () => {
     const { data: { data } } = await axios.post('http://localhost:5000/api/auth/user', { token: sessionStorage.getItem('token') });
-    id = data._id
-    const res = await axios.post('http://localhost:5000/api/todo/allTodos', { userId: id, token: sessionStorage.getItem('token') });
+    const res = await axios.post('http://localhost:5000/api/todo/allTodos', { userId: data._id, token: sessionStorage.getItem('token') });
     setAllTodos(res.data.todos)
   }
-  
+
   const handleAddTask = async () => {
     if (task !== '') {
       const { data: { data } } = await axios.post('http://localhost:5000/api/auth/user', { token: sessionStorage.getItem('token') });
@@ -26,29 +24,29 @@ const Todo = () => {
 
   useEffect(() => {
     fetchAllTask();
-  }, [])
+  }, [allTodos])
+
 
 
   return (
-    <div className='h-full p-10 flex flex-col items-center'>
-      <div className='max-w-4xl w-full'>
+    <div className='h-full md:p-10 p-2 flex flex-col items-center'>
+      <div className='max-w-3xl w-full'>
         <div className='flex gap-1'>
           <input value={task} onChange={(e) => setTask(e.target.value)} type="text" className='w-full h-16 px-3 rounded-md border-2' placeholder='Enter your task' />
           <button onClick={handleAddTask} className='px-5 rounded-md font-semibold bg-yellow-400'>Add</button>
         </div>
         {
           allTodos.length !== 0 ?
-            <div className="mt-10 flex flex-col items-start w-full">
+            <div className="mt-10 mb-16 flex flex-col items-start w-full">
               {allTodos.length !== 0 && allTodos.map((todo) => (
                 <Task key={todo.todo} todo={todo} fun={fetchAllTask} />
               ))}
             </div>
             :
             <div className='mt-10 text-center'>No Tasks Found</div>
-          }
+        }
       </div>
     </div>
   )
 }
-
 export default Todo
