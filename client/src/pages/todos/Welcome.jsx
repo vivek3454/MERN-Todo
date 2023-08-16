b05a70baa9024b0bc0ce482f63cb2a98a9f838fc
Vivek <vivekparde2411@gmail.com>
2023-08-16 18:11:23 +0530

    added welcome page

---
+++ client/src/pages/todos/Welcome.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Welcome = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!sessionStorage.getItem('token')) {
+      navigate('/signin')
+    }
+  }, [])
+  return (
+    <div className='text-3xl font-semibold flex items-center max-w-md mx-auto text-center h-full'>
+      <div>
+        Welcome to <span className='text-yellow-400'>Todoblocks</span> a MERN based Todo app
+      </div>
+    </div>
+  )
+}
+
+export default Welcome
