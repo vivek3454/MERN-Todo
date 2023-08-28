91b7fc612c9d3a19d9a0ace6de16bdde83c63e02
Vivek <vivekparde2411@gmail.com>
2023-08-28 21:59:27 +0530

    updated api route method

--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {jwtAuth} = require('../middleware/user.middleware');
-const { createTask, getAllTodos, deleteTask, updateTask, getPendingTask, getPendingTasks, getcompletedTasks, updateStatus } = require('../controllers/todo.controller');
+const { createTask, getAllTodos, deleteTask, updateTask, getPendingTasks, getcompletedTasks, updateStatus } = require('../controllers/todo.controller');
 const todoRouter = express.Router();
 
 todoRouter.post('/create', jwtAuth, createTask);
 todoRouter.post('/allTodos', jwtAuth, getAllTodos);
-todoRouter.delete('/delete', jwtAuth, deleteTask);
+todoRouter.post('/delete', jwtAuth, deleteTask);
 todoRouter.put('/update', jwtAuth, updateTask);
 todoRouter.post('/pending', jwtAuth, getPendingTasks);
 todoRouter.post('/completed', jwtAuth, getcompletedTasks);
